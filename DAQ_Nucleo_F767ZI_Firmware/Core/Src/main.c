7aad0d657936883c43f4d563a0d06905c9bc80c6
Graham Driver <51301220+GrahamDrive@users.noreply.github.com>
2025-11-02 16:47:57 -0600

    FreeRTOS Added

--- DAQ_Nucleo_F767ZI_Firmware/Core/Src/main.c
+++ DAQ_Nucleo_F767ZI_Firmware/Core/Src/main.c
@@ -17,13 +17,16 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
+#include <task_usb.h>
+#include <task_usb.h>
+#include <task_usb.h>
 #include "main.h"
 #include "string.h"
+#include "cmsis_os.h"
 #include "usb_device.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "usbd_cdc_if.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -65,6 +68,30 @@ ETH_HandleTypeDef heth;
 
 UART_HandleTypeDef huart3;
 
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .stack_size = 256 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for USBTask */
+osThreadId_t USBTaskHandle;
+const osThreadAttr_t USBTask_attributes = {
+  .name = "USBTask",
+  .stack_size = 512 * 4,
+  .priority = (osPriority_t) osPriorityRealtime,
+};
+/* Definitions for USBTXQueue */
+osMessageQueueId_t USBTXQueueHandle;
+const osMessageQueueAttr_t USBTXQueue_attributes = {
+  .name = "USBTXQueue"
+};
+/* Definitions for USBRXQueue */
+osMessageQueueId_t USBRXQueueHandle;
+const osMessageQueueAttr_t USBRXQueue_attributes = {
+  .name = "USBRXQueue"
+};
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -74,6 +101,9 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_ETH_Init(void);
 static void MX_USART3_UART_Init(void);
+void StartDefaultTask(void *argument);
+extern void USB_Task(void *argument);
+
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -114,25 +144,60 @@ int main(void)
   MX_GPIO_Init();
   MX_ETH_Init();
   MX_USART3_UART_Init();
-  MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 2 */
   
   /* USER CODE END 2 */
 
+  /* Init scheduler */
+  osKernelInitialize();
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* Create the queue(s) */
+  /* creation of USBTXQueue */
+  USBTXQueueHandle = osMessageQueueNew (16, sizeof(uint16_t), &USBTXQueue_attributes);
+
+  /* creation of USBRXQueue */
+  USBRXQueueHandle = osMessageQueueNew (16, sizeof(uint16_t), &USBRXQueue_attributes);
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* creation of USBTask */
+  USBTaskHandle = osThreadNew(USB_Task, NULL, &USBTask_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
+
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-
-  uint8_t buffer[4096];
-
-  for(int i = 0; i < sizeof(buffer); i++){
-	  buffer[i] = i & 0xFF;
-  }
-
   while (1)
   {
-	  if(CDC_Transmit_FS(buffer, sizeof(buffer)) != USBD_OK){
-		  asm("NOP");
-	  }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -341,6 +406,26 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* init code for USB_DEVICE */
+  MX_USB_DEVICE_Init();
+  /* USER CODE BEGIN 5 */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END 5 */
+}
 
 /**
   * @brief  Period elapsed callback in non blocking mode
