a1e992b33906af361baad59dba2ddd8cfd9234f4
Graham Driver <51301220+GrahamDrive@users.noreply.github.com>
2025-11-09 22:34:44 -0600

    Update usbSpeedTest.py
    The test script is now able to send commands to the DAQ

--- TestingSoftware/usbSpeedTest.py
+++ TestingSoftware/usbSpeedTest.py
@@ -1,17 +1,39 @@
 import serial, time
+import threading
+
 
 port = "COM8"
 ser = serial.Serial(port, baudrate=115200, timeout=0)
 
-total = 0
-start = time.perf_counter()
 
-while True:
-    data = ser.read(65536)
-    total += len(data)
-    elapsed = time.perf_counter() - start
-    if elapsed >= 1.0:
-        mbps = (total * 8) / 1_000_000 / elapsed  # megabits/s
-        print(f"{mbps:.2f} Mb/s")
-        total = 0
-        start = time.perf_counter()
+def read(ser: serial.Serial):
+    print("reading start")
+    total = 0
+    start = time.perf_counter()
+
+    while True:
+        data = ser.read(65536)
+        total += len(data)
+        elapsed = time.perf_counter() - start
+        if elapsed >= 1.0:
+            mbps = (total * 8) / 1_000_000 / elapsed  # megabits/s
+            print(f"{mbps:.2f} Mb/s")
+            total = 0
+            start = time.perf_counter()
+
+def write(ser: serial.Serial):
+    print("write start")
+    while True:
+        command = bytes([int(input())])
+        print(f"sending {command}")
+        ser.write(command)
+
+def main():
+
+    read_thread = threading.Thread(target=read, args=(ser,))
+    read_thread.start()
+
+    write_thread = threading.Thread(target=write, args=(ser,))
+    write_thread.start()
+
+main()
