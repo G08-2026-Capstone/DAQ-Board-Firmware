c7d8877b79be66255f4437e2e306a34b9e62007e
Graham Driver <51301220+GrahamDrive@users.noreply.github.com>
2025-11-12 14:04:12 -0600

    Sending Entire Buffer In Queue

--- DAQ_Nucleo_F767ZI_Firmware/Core/Src/Tasks/task_usbTX.c
+++ DAQ_Nucleo_F767ZI_Firmware/Core/Src/Tasks/task_usbTX.c
@@ -10,22 +10,14 @@
 #define BUFFER_SIZE 800
 
 void TASK_USB_BULK_TX(void *argument){
-	uint32_t message = 0;
+
 	uint32_t buffer[BUFFER_SIZE];
-	uint16_t bufferPointer = 0;
 	for(;;){
         // Wait for there to be data to send over the USB line
-		osMessageQueueGet(USBTXQueueHandle, &message, NULL, osWaitForever);
+		osMessageQueueGet(USBTXQueueHandle, &buffer, NULL, osWaitForever);
 
 		// Data will only be sent when the sending buffer is full
-		if(bufferPointer >= BUFFER_SIZE){
-			while(CDC_Transmit_FS((uint8_t*) &buffer, sizeof(uint32_t)*BUFFER_SIZE) != USBD_OK);
-			bufferPointer = 0;
-		}
-		else{	// Filling the buffer
-			buffer[bufferPointer] = message;
-			bufferPointer++;
-		}
+		while(CDC_Transmit_FS((uint8_t*) &buffer, sizeof(uint32_t)*BUFFER_SIZE) != USBD_OK);
 
 	}
 
