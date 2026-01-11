8bcc08e3990e0dfaa76ef08a2f6f40afe1fe4e27
Graham Driver <51301220+GrahamDrive@users.noreply.github.com>
2026-01-11 17:03:42 -0600

    Update USB TX task header and add ADC data repackaging
    Corrected include guard names in task_usbTX.h, added main.h include, declared external DMA RX buffer, and introduced the repackage_ADC_Data function prototype.

--- DAQ_Nucleo_F767ZI_Firmware/Core/Inc/Tasks/task_usbTX.h
+++ DAQ_Nucleo_F767ZI_Firmware/Core/Inc/Tasks/task_usbTX.h
@@ -5,24 +5,26 @@
  *      Author: drive
  */
 
-#ifndef INC_TASKS_TASK_USB_H_
-#define INC_TASKS_TASK_USB_H_
+#ifndef INC_TASKS_TASK_USBTX_H_
+#define INC_TASKS_TASK_USBTX_H_
 
 // Includes
 #include "cmsis_os.h"
 #include "task.h"
 #include "usbd_cdc_if.h"
 #include "usb_device.h"
+#include "main.h"
 
 
 extern osThreadId_t usbTXBulkHandle;
 extern osMessageQueueId_t USBTXQueueHandle;
-
-// Defines
-#define BUFFER_SIZE 10000
+extern uint8_t ads131mo4_DMA_rx_buffer[BUFFER_SIZE_BYTES];
 
 // Functions
 void TASK_USB_BULK_TX(void *argument);
 
 
-#endif /* INC_TASKS_TASK_USB_H_ */
+void repackage_ADC_Data();
+
+
+#endif /* INC_TASKS_TASK_USBTX_H_ */
